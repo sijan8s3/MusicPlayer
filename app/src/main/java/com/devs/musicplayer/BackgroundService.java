40c6059d414d61253b77c14c476cd522bd476bb6
JOKASTA <sijanneupane12@gmail.com>
2020-08-30 07:40:44 +0545

    first commit

---
+++ app/src/main/java/com/devs/musicplayer/BackgroundService.java
@@ -0,0 +1,21 @@
+package com.devs.musicplayer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.core.app.NotificationCompat;
+
+public class BackgroundService extends Service {
+    private static final int NOTIF_ID= 1;
+    private static final  String NOTIFI_CHANNEL_ID= "Devs_Player";
+
+    public BackgroundService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return  null;
+    }
+}
